ba5bcb7918314082c32638dde4c3438253640147
ahmedmoheb9710 <moheb413@gmail.com>
2021-02-17 21:56:17 +0200

    chatbot

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:chat_bot/login.dart';
+import 'package:flutter/material.dart';
+void main() {
+
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+
+
+
+
+  runApp(MaterialApp(
+    home: login(),
+  ));
+}
+
